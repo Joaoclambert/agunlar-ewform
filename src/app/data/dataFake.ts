0d0c8acf4486595e955b04562ece9e92f2a40dd3
Joao Lambert <joaoclambert@hotmail.com>
2022-12-07 21:28:52 -0300

    house reform

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+import { identifierName } from "@angular/compiler";
+
+export const dataFake = [
+   { "id" : "1",
+    "title": "Casa Catas Altas em reparos.",
+    "description": "Reparo sendo feito no telhado dos fundos",
+    "photoCover":"assets/reform.jpg"
+     
+   }, 
+   {
+    "id" : "2",
+    "title": "tttttt22",
+    "description": "dddddd22",
+    "photoCover":"assets/reform1.jpg"
+
+   },
+   {
+
+      "id" : "3",
+      "title": "tttttt33",
+      "description": "dddddd33",
+      "photoCover":"assets/reform1.jpg"
+  
+   },
+
+   {
+
+      "id" : "4",
+      "title": "tttttt44",
+      "description": "dddddd44",
+      "photoCover":"assets/reform1.jpg"
+
+
+
+
+   }
+
+
+]
+
+
+
